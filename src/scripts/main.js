c80a1016f5316960752436539a55e8e271f9a135
Jisie DM <jisie.david@gmail.com>
2021-01-21 12:30:55 -0800

    Creating 5 people in main.js with testPerson

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,11 @@ import { testPerson } from "./TestFacility.js"
 
 // Create and test 5 people at the testing facility
 let doug = testPerson("Doug", 25, 99);
-console.log(doug)
+let bob = testPerson("Bob", 26, 100);
+let lisa = testPerson("Lisa", 27, 101);
+let sally = testPerson("Sally", 28, 102);
+let martin = testPerson("Martin", 29, 103);
+// console.log(doug)
 
 // Diagnose each person at the clinic
 // doug = diagnose();
